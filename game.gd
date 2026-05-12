5f8ea779a7ef380a55e9187e1581bd8faee88829
Theodore <andreaskok446@gmail.com>
2026-05-12 18:15:27 +0300

    animations sto node eucrasius

--- game.gd
+++ game.gd
@@ -1,4 +1,6 @@
 extends Node2D
+var pause_scene = preload("res://pause.tscn")
+var pause_menu_instance = null
 
 const MAX_MOBS = 10
 
@@ -41,4 +43,16 @@ func _process(_delta):
 		var minutes = time_left / 60
 		var seconds = time_left % 60
 		time_label.text = "%02d:%02d" % [minutes, seconds]
-		
+		if Input.is_action_just_pressed("ui_cancel"):
+			toggle_pause()
+func toggle_pause():
+	if get_tree().paused:
+		get_tree().paused = false
+		if pause_menu_instance:
+			pause_menu_instance.queue_free()
+			pause_menu_instance = null
+	else:
+		get_tree().paused = true
+		if pause_menu_instance == null:
+			pause_menu_instance = pause_scene.instantiate()
+			get_tree().root.add_child(pause_menu_instance)
