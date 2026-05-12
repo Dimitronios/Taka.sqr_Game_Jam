7837b9ad7ced1e309d633e963f922fbd8a2262f9
Dimitronios <dimispap.thess@gmail.com>
2026-05-12 16:46:02 +0300

    FIXED pause menu kai logo

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,5 +1,5 @@
 extends Control
-@onready var PauseMenu = $PauseMenu
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -25,4 +25,5 @@ func _on_quit_pressed() -> void:
 
 func _on_main_menu_pressed() -> void:
 	get_tree().paused = false
+	visible = false
 	get_tree().change_scene_to_file("res://main_menu.tscn")
